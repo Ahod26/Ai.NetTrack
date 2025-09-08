1f38cc7f30d8b116de81762d9e7feb28861302de
ArbelHodesman <arbelhodesman@gmail.com>
2025-09-08 16:20:42 +0300

    v

--- backend/Services/Classes/NewsAggregation/NewsCollectorService.cs
+++ backend/Services/Classes/NewsAggregation/NewsCollectorService.cs
@@ -2,10 +2,13 @@ using backend.Services.Interfaces.NewsAggregation;
 
 namespace backend.Services.Classes.NewsAggregation;
 
-public class NewsCollectorService(IGitHubService gitHubService) : INewsCollectorService
+public class NewsCollectorService(
+  IGitHubService gitHubService,
+  IYouTubeService youTubeService) : INewsCollectorService
 {
   public async Task CollectAllNews()
   {
     await gitHubService.GetGitHubAIUpdatesAsync();
+    await youTubeService.GetYouTubeAIUpdatesAsync();
   }
 }
--- backend/Services/Interfaces/NewsAggregation/IYouTubeService.cs
+++ backend/Services/Interfaces/NewsAggregation/IYouTubeService.cs
@@ -1,6 +1,8 @@
+using backend.Models.Domain;
+
 namespace backend.Services.Interfaces.NewsAggregation;
 
 public interface IYouTubeService
 {
-  
+  Task<List<NewsItem>> GetYouTubeAIUpdatesAsync();
 }
